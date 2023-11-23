7746561c2a16d7405978716a308dc0aa15351082
martin-erlich-deel <64599962+martin-erlich-deel@users.noreply.github.com>
2023-11-22 22:22:11 -0300

    add heroku configs

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def get_zona_prop():
     url = body['url']
     return jsonify(get_zona_prop_info(url))
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0')
-    app.run(host='0.0.0.0',debug=True,port=3000)
+    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0',debug=True,port=3000)
