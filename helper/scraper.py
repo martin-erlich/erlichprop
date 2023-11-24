23778dc4d10209f3524b01cfd635ef200b671cc8
martin-erlich-deel <64599962+martin-erlich-deel@users.noreply.github.com>
2023-11-24 14:21:04 -0300

    Update scraper.py

--- helper/scraper.py
+++ helper/scraper.py
@@ -5,15 +5,15 @@ import os
 from seleniumbase import Driver
 from seleniumbase import page_actions
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 
 def get_zona_prop_info(url):
 
-    chrome_options = webdriver.ChromeOptions()
+    chrome_options = Options()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     dr = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
